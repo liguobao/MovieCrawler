f74c2de0b6b2acff54f36a4a7be8a27e29cdfc71
liguobao <codelover@qq.com>
2018-01-16 22:51:02 +0800

    check code

--- Dy2018CrawlerForJson/Jobs/AutoGetMovieListJob.cs
+++ Dy2018CrawlerForJson/Jobs/AutoGetMovieListJob.cs
@@ -5,7 +5,7 @@ namespace Dy2018CrawlerForJson.Jobs
     public class AutoGetMovieListJob:Job
     {
        
-        [Invoke(Begin = "2017-03-10 00:30", Interval = 1000 * 3600*3, SkipWhileExecuting =true)]
+        [Invoke(Begin = "2018-01-16 00:30", Interval = 1000 * 3600*3, SkipWhileExecuting =true)]
         public void Run()
         {
             LogHelper.Info("Start crawling");
